c26e8605753f254dac76f57ee5cc73d8e0c71691
aydinuslu <uslu.aydin@gmail.com>
2024-07-15 15:25:00 +0300

    video #3 dynamic classes

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = Vue.createApp({
             url: 'http://www.thenetninja.co.uk',
             showBooks: true,
             books: [
-                {title: 'Name of the Wind', author: 'Patrick Rothfuss',img: 'assets/1.jpg'},
-                {title: 'The Final Empire', author: 'Brandon Sanderson',img: 'assets/3.jpg'},
-                {title: 'The Way of Kings', author: 'Brandon Sanderson',img: 'assets/2.jpg'}
+                {title: 'Name of the Wind', author: 'Patrick Rothfuss',img: 'assets/1.jpg',isFav: true},
+                {title: 'The Final Empire', author: 'Brandon Sanderson',img: 'assets/3.jpg',isFav: true},
+                {title: 'The Way of Kings', author: 'Brandon Sanderson',img: 'assets/2.jpg',isFav: false}
             ]
         }        
     },
