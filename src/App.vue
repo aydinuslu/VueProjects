fa4025801b0986c7056a9f3605948e093569cb9e
aydinuslu <uslu.aydin@gmail.com>
2024-07-19 17:37:31 +0300

    video 6 - new game

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <!-- The video I am watching at:
+   https://youtu.be/bc6czIBLKTg?list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&t=644-->
+   <Block v-if="isPlaying" v-bind:delay="delay"/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import Block from './components/Block.vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Block},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    start() {
+      this.isPlaying = true;
+      this.delay = Math.floor(Math.random() * 5000) + 2000;
+      console.log(this.delay);      
+    }
   }
 }
+
 </script>
 
 <style>
@@ -20,7 +35,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
